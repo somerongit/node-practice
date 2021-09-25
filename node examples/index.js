4376fd76098ae1720df566bec365d6e823cdff24
somerongit <someron.bakuli05@gmail.com>
2021-09-25 07:04:38 +0530

    Callback

--- node examples/index.js
+++ node examples/index.js
@@ -1,18 +1,21 @@
 const rect=require("./rectangle");
 
-function solveRect(l,b) {
-    console.log( 'color: #40dd20',"Hi hello nice to see you baby "+b);
+function solveRect(l,b,t) {
     console.info("\nSolving for rectangle with l= "+l+" and b = "+b);
-    if (l<=0 || b<=0) {
-        console.error("\nRectangle dimentions should be grater then Zero!!!");
-    }else{
-        console.log("\n\tThe area of rectangle is "+rect.area(l,b));
-        console.log("\n\tThe perimeter of rectangle is "+rect.perimeter(l,b));
-
-    }
+    rect(l,b,t, (err, rectangle)=>{
+        if(err){
+            console.log("\nError: ",err.message);
+        }else{
+            console.log("\nThe area of the rectangle of dimesions L: "
+            +l+" and B: "+b+" is:"+rectangle.area());
+            console.log("The perimeter of the rectangle of dimesions L: "
+            +l+" and B: "+b+" is:"+rectangle.perimeter());
+        }
+    });
+    console.log("This satemet is after call of rect");
 }
 
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
+solveRect(2,4,2000);
+solveRect(3,5,3000);
+solveRect(0,5,2000);
+solveRect(-3,5,3000);
--- node examples/rectangle.js
+++ node examples/rectangle.js
@@ -1,2 +1,17 @@
-exports.perimeter=(x,y)=>(2*(x+y));
-exports.area=(x,y)=>(x*y);
+module.exports = (x,y,t,callback) => {
+    if(x<=0||y<=0){
+        setTimeout(()=>
+        callback(new Error("\nRectangle dimensions shoulbe be grater then zero, the value of L is: "+
+        x+" and the value of B is: "+y),null),
+        t);
+    }else{
+        setTimeout(()=>
+        callback(null,
+            {
+                perimeter:()=>(2*(x+y)),
+                area:()=>(x*y)
+            }),
+            t);
+    }
+
+}
