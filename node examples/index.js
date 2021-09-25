792e5e84e925477952f192ec34b0a2a07840354a
somerongit <someron.bakuli05@gmail.com>
2021-09-25 08:07:26 +0530

    Docs added

--- node examples/index.js
+++ node examples/index.js
@@ -1,8 +1,8 @@
 const rect=require("./rectangle");
 
-function solveRect(l,b,t) {
+function solveRect(l,b) {
     console.info("\nSolving for rectangle with l= "+l+" and b = "+b);
-    rect(l,b,t, (err, rectangle)=>{
+    rect(l,b, (err, rectangle)=>{
         if(err){
             console.log("\nError: ",err.message);
         }else{
@@ -15,7 +15,7 @@ function solveRect(l,b,t) {
     console.log("This satemet is after call of rect");
 }
 
-solveRect(2,4,2000);
-solveRect(3,5,3000);
-solveRect(0,5,2000);
-solveRect(-3,5,3000);
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
--- node examples/rectangle.js
+++ node examples/rectangle.js
@@ -1,9 +1,9 @@
-module.exports = (x,y,t,callback) => {
+module.exports = (x,y,callback) => {
     if(x<=0||y<=0){
         setTimeout(()=>
         callback(new Error("\nRectangle dimensions shoulbe be grater then zero, the value of L is: "+
         x+" and the value of B is: "+y),null),
-        t);
+        2000);
     }else{
         setTimeout(()=>
         callback(null,
@@ -11,7 +11,7 @@ module.exports = (x,y,t,callback) => {
                 perimeter:()=>(2*(x+y)),
                 area:()=>(x*y)
             }),
-            t);
+            2000);
     }
 
 }
