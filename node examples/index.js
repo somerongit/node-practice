e3b942daf4e818a19c7a85a6c44a0796dfd777c8
somerongit <someron.bakuli05@gmail.com>
2021-09-25 22:00:26 +0530

    Express Status Code :200

--- node examples/index.js
+++ node examples/index.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-     http = require('http');
+ http = require('http');
 const morgan = require('morgan');
 const hostname = 'localhost';
 const port = 3000;
@@ -11,15 +11,14 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
 });
 
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
